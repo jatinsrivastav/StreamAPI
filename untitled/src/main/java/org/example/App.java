aa54d15b69509ed184a3cdf729bc9c67a8d471fc
jatin <jatinsrivastav.mrj@gmail.com>
2023-07-29 23:35:06 +0530

    map implemenatation

--- untitled/src/main/java/org/example/App.java
+++ untitled/src/main/java/org/example/App.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.function.Function;
 
 /**
  * Hello world!
@@ -11,7 +11,18 @@ import java.util.stream.Stream;
 public class App 
 {
     public static void main( String[] args )
+
     {
+        Function <Integer,Integer> func= new Function<Integer,Integer>() {
+            @Override
+            public Integer apply(Integer o) {
+                return o*2;
+            }
+        };
+
+        List<Integer> data2= Arrays.asList(4,9,5,7);
+        data2.stream().map(func).forEach(n->System.out.println(n));
+        System.out.println("--------");
         List<Integer> data= Arrays.asList(6,9,5,1);
         data.stream().map(n->n*2).forEach(n->System.out.println(n));
 
