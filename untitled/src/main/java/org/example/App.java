44fa0c93ec71be0de3154f2c6dd101f06df78723
jatin <jatinsrivastav.mrj@gmail.com>
2023-07-29 21:05:22 +0530

    3 extra lines

--- untitled/src/main/java/org/example/App.java
+++ untitled/src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<Integer> num = Arrays.asList(3,87,9);
+        Stream<Integer> data= num.stream();
+         Stream <Integer> data2=data.map(n->n*2);
+
+        data2.forEach(n->System.out.println(n));
+
+
     }
 }
